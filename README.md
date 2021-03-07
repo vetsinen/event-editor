3daa707cf3aa19f37461e09f3474a072b78fa1aa
vetsinen <vetsinen@gmail.com>
2021-03-07 15:26:37 +0200

    save data to cloud

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Strapi application
 
+sucasa@yopmail.com
+
+
 A quick description of your strapi application
--- config/database.js
+++ config/database.js
@@ -1,15 +1,66 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', '38.17.53.112'),
+        port: env.int('DATABASE_PORT', 22402),
+        database: env('DATABASE_NAME', 'sucasa'),
+        username: env('DATABASE_USERNAME', 'sucasa'),
+        password: env('DATABASE_PASSWORD', 'welcomeToStrapi123'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {}
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
+// Username: EjMa1aYQAE
+//
+// Database name: EjMa1aYQAE
+//
+// Password: eBA08jkTwv
+//
+// Server: remotemysql.com
+//
+// Port: 3306
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('DATABASE_HOST', '38.17.53.112'),
+//         port: env.int('DATABASE_PORT', 22402),
+//         database: env('DATABASE_NAME', 'sucasa'),
+//         username: env('DATABASE_USERNAME', 'sucasa'),
+//         password: env('DATABASE_PASSWORD', 'welcomeToStrapi123'),
+//       },
+//       options: {
+//         ssl: true
+//       },
+//     },
+//   },
+// });
+
+
